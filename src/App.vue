5729a52fde8729bcd1783ff576979c89214808ef
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-03-27 19:24:10 +0200

    still work on structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script>
-
+<script setup>
+import MainHeader from './components/MainHeader.vue'
+import MainPage from './components/MainPage.vue'
+import BlueBan from './components/BlueBan.vue'
+import FooterBan from './components/FooterBan.vue'
+import MainFooter from './components/MainFooter.vue'
 </script>
 
-<template></template>
+<template>
+  <MainHeader />
+  <MainPage />
+  <BlueBan />
+  <FooterBan />
+  <MainFooter />
+</template>
 
-<style></style>
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
